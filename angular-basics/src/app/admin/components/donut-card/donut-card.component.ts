98f858656f121c56c02e33dde33bb8ef4fb2734e
piyushsh19 <piyush.sh19@gmail.com>
2024-01-05 22:18:20 +0530

    host element

--- angular-basics/src/app/admin/components/donut-card/donut-card.component.ts
+++ angular-basics/src/app/admin/components/donut-card/donut-card.component.ts
@@ -4,7 +4,7 @@ import { Donut } from '../../models/donut.model';
 @Component({
   selector: 'donut-card',
   template: `
-    <div class="donut-card">
+ 
       <img
         src="/assets/img/{{ donut.icon }}.svg"
         [alt]="donut.name"
@@ -17,13 +17,13 @@ import { Donut } from '../../models/donut.model';
         <p class="donut-card-price">
           {{ donut.price }}
         </p>
-      </div>
-    </div>
+
   `,
   styles: [
     `
-      .donut-card {
-        display: flex;
+    // Notye -  we need to have the display flex or block a custom elemt which angular implement is donut card. which are not by default gets any display. But we get the benefit of having a div on host elemnt as its uses usseragent sheet which has property of block
+    :host{
+       display: flex;
         align-items: center;
         background: #f7f7f7;
         border-radius: 5px;
@@ -33,6 +33,10 @@ import { Donut } from '../../models/donut.model';
         &:hover {
           transform: translateY(-3px);
         }
+    }
+    // :host is not angular specific it i shadow dom specification which is a feature of new platform
+      .donut-card {
+        
         &-name {
           font-size: 16px;
         }
